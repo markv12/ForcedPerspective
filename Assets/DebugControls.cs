60f03a1d91e12466b2c6edb80413a6a49ffa3c55
Scott Steffes <scott_steffes@capsulatedsoftware.com>
2017-11-12 20:34:56 +0900

    Initial Commit

---
+++ Assets/DebugControls.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class DebugControls : MonoBehaviour {
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            SceneManager.LoadScene("Main");
+            
+        } else if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.Q)) {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+	}
+}
---
+++ Assets/ObjectGrabber.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class ObjectGrabber : MonoBehaviour {
+
+    public Transform mainTransform;
+    public Transform grabber;
+
+    private LayerMask grabbableMask;
+    private LayerMask environmentMask;
+
+    void Awake() {
+        environmentMask = LayerMask.GetMask("Environment");
+        grabbableMask = LayerMask.GetMask("Grabbable");
+    }
+
+    Transform currentObject = null;
+
+    void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            if (currentObject == null) {
+                RaycastHit hit;
+                if (Physics.Raycast(mainTransform.position, mainTransform.forward, out hit, 1000, grabbableMask.value)) {
+                    grabber.position = hit.point;
+                    hit.transform.SetParent(grabber, true);
+                    currentObject = hit.transform;
+                    Destroy(hit.rigidbody);
+                    currentObject.GetComponentInChildren<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+                }
+            } else {
+                currentObject.gameObject.AddComponent<Rigidbody>();
+                currentObject.GetComponentInChildren<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+                currentObject.SetParent(null, true);
+                currentObject = null;
+            }
+        }
+        if(currentObject != null) {
+            RaycastHit hit;
+            if (Physics.Raycast(mainTransform.position, mainTransform.forward, out hit, 1000, environmentMask.value)) {
+                float previousDistance = Vector3.Distance(grabber.position, mainTransform.position);
+                Vector3 newPostion = hit.point;
+                Vector3 relativePosition = newPostion - mainTransform.position;
+                relativePosition *= 0.6f;
+                newPostion = mainTransform.position + relativePosition;
+
+                float newDistance = Vector3.Distance(newPostion, mainTransform.position);
+                float distanceChange = newDistance / previousDistance;
+                grabber.position = newPostion;
+                currentObject.localPosition *= distanceChange;
+                currentObject.transform.localScale *= distanceChange;
+            }
+        }
+	}
+}
